e52d47ab366429079334557fb7d5bc1f81519169
Volpeffervescente <martinafortuna2002@gmail.com>
2024-07-20 12:53:09 +0200

    added test file and fixed part of bitmap.c

--- bit_map.c
+++ bit_map.c
@@ -19,7 +19,7 @@ void BitMap_setBit(BitMap* bit_map, int bit_num, int status){
   // get byte
   int byte_num=bit_num>>3;
   assert(byte_num<bit_map->buffer_size);
-  int bit_in_byte=byte_num&0x03;
+  int bit_in_byte=byte_num&0x07;
   if (status) {
     bit_map->buffer[byte_num] |= (1<<bit_in_byte);
   } else {
@@ -31,6 +31,6 @@ void BitMap_setBit(BitMap* bit_map, int bit_num, int status){
 int BitMap_bit(const BitMap* bit_map, int bit_num){
   int byte_num=bit_num>>3; 
   assert(byte_num<bit_map->buffer_size);
-  int bit_in_byte=byte_num&0x03;
+  int bit_in_byte=byte_num&0x07;
   return (bit_map->buffer[byte_num] & (1<<bit_in_byte))!=0;
 }
